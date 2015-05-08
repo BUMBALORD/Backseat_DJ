ed5e29f390c00210a14661bb091c0948e4d8d16f
skwongg <silaskwong1@gmail.com>
2015-05-07 18:19:37 -0700

    initial commit

--- backend-rails/app/controllers/application_controller.rb
+++ backend-rails/app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::API
+
+    def index
+       # register a client with YOUR_CLIENT_ID as client_id_
+        client = SoundCloud.new(:client_id => YOUR_CLIENT_ID)
+        # get 10 hottest tracks
+        tracks = client.get('/tracks', :limit => 10, :order => 'hotness')
+        # print each link
+        tracks.each do |track|
+          puts track.permalink_url
+        end
+
+
+        response = HTTParty.get("https://api.github.com/zen",
+        {headers:
+          {"User-Agent" => "Malibootay",
+            "Authorization" => 'token ' + ENV['key']
+            }
+          })
+        @zen = response.parsed_response
+
+    end
+
+
+
+
 end
