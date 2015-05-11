f6809ed1c9ff0303cf8d02def300716349642e18
Katrina Eaton <katrina.eaton@gmail.com>
2015-05-11 09:57:33 -0700

    mystery data

--- backend-rails/db/migrate/20150508003019_create_songs.rb
+++ backend-rails/db/migrate/20150508003019_create_songs.rb
@@ -2,7 +2,6 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title    ##Soundcloud outputs weren't helpful
-      # t.string :artists
       t.integer :track_id
       t.belongs_to :playlist
       t.integer :skip, default: 0
